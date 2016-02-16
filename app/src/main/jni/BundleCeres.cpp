f4c513a17751cfe93dd61296fba629d6c6f1329c
Kunat Pipatanakul <kunat.pi@gmail.com>
2016-02-16 17:46:56 +0900

    Reflector Bundle to using eigen.

--- app/src/main/jni/BundleCeres.cpp
+++ app/src/main/jni/BundleCeres.cpp
@@ -9,20 +9,21 @@
 #include "ceres/rotation.h"
 #include "Eigen/Dense"
 #include "Eigen/Geometry"
+#include "cmath"
 
 using namespace cv;
 using namespace std;
 
-typedef Eigen::Matrix<double,3,3> Matrix3d;
-typedef Eigen::Matrix<double,3,1> Vector3d;
+typedef Eigen::Matrix<double,3,3,Eigen::ColMajor> Matrix3d;
+typedef Eigen::Matrix<double,3,1,Eigen::ColMajor> Vector3d;
 struct ReprojectionErrorData
 {
     vector<Point2f> points;
     vector<int> matches_image_idx;
 };
 struct ReprojectionError {
-    ReprojectionError(double x1,double y1 , double x2, double y2,double *K1,double *K2)
-            : x1(x1), y1(y1) , x2(x2) , y2(y2) , K1(K1), K2(K2){}
+    ReprojectionError(const double *p1,const double *p2,const double *K1,const double *K2)
+            : p1(p1), p2(p2) , K1(K1), K2(K2){}
     bool operator()(const double* rvec1,const double* rvec2,double* residuals) const {
         double R1[9];
         double R2[9];
@@ -34,12 +35,19 @@ struct ReprojectionError {
         Matrix3d eigen_KMat2;
         Matrix3d eigen_KInv1;
         Matrix3d eigen_KInv2;
+        Vector3d eigen_p1;
+        Vector3d eigen_p2;
+        Vector3d out1;
+        Vector3d out2;
+//        cout << x1 << ","<<y1<< " ; " << x2 << "," << y2 << endl;
         eigen_KMat1 << *(K1+0), 0.0, *(K1+2),
                 0.0, *(K1+4), *(K1+5),
                 0.0,  0.0, 1.0;
         eigen_KMat2 << *(K2+0), 0.0, *(K2+2),
                 0.0, *(K2+4), *(K2+5),
                 0.0,  0.0, 1.0;
+        eigen_p1 << *(p1+0),*(p1+1),*(p1+2);
+        eigen_p2 << *(p2+0),*(p2+1),*(p2+2);
         ceres::AngleAxisToRotationMatrix(rvec1,R1);
         ceres::AngleAxisToRotationMatrix(rvec2,R2);
         //order of matrix R
@@ -48,73 +56,40 @@ struct ReprojectionError {
         //	 2 5 8 ]
         eigen_R1 << R1[0],R1[3],R1[6],R1[1],R1[4],R1[7],R1[2],R1[5],R1[8];
         eigen_R2 << R2[0],R2[3],R2[6],R2[1],R2[4],R2[7],R2[2],R2[5],R2[8];
-
+//        eigen_R1 << R1[0],R1[1],R1[2],R1[3],R1[4],R1[5],R1[6],R1[7],R1[8];
+//        eigen_R2 << R2[0],R2[1],R2[2],R2[3],R2[4],R2[5],R2[6],R2[7],R2[8];
 
         eigen_KInv1 = eigen_KMat1.inverse();
         eigen_KInv2 = eigen_KMat2.inverse();
-        //R * K^t(K^-1) = H
-        //H*[x,y,1]
         //Then find difference between x,y,z
         eigen_H1 = eigen_R1 * eigen_KInv1;
         eigen_H2 = eigen_R2 * eigen_KInv2;
-        // THIS IS FOR not using eigen lib.
-        //		for(int i = 0 ; i < 3 ; i ++){
-        //			for(int j = 0 ; j < 3 ;j ++){
-        //				double sum1(0);
-        //				double sum2(0);
-        //				for(int k = 0 ; k < 3 ; k++){
-        //					//					sum1+=(R1[i+k*3]*K1[k*3+j]);
-        //					//					sum2+=(R2[i+k*3]*K2[k*3+j]);
-        //					sum1+=(R1[i+k*3]*K_inv1(k,j));
-        //					sum2+=(R2[i+k*3]*K_inv2(k,j));
-        //				}
-        //				H2[i*3+j] = sum2;
-        //				H1[i*3+j] = sum1;
-        //			}
-        //		}
-        //		double dx1 = H1[0]*x1 + H1[1]*y1 + H1[2];
-        //		double dy1 = H1[3]*x1 + H1[4]*y1 + H1[5];
-        //		double dz1 = H1[6]*x1 + H1[7]*y1 + H1[8];
-        //		double dx2 = H2[0]*x2 + H2[1]*y2 + H2[2];
-        //		double dy2 = H2[3]*x2 + H2[4]*y2 + H2[5];
-        //		double dz2 = H2[6]*x2 + H2[7]*y2 + H2[8];
-        double dx1 = eigen_H1(0,0) * x1 + eigen_H1(0,1)*y1 + eigen_H1(0,2);
-        double dy1 = eigen_H1(1,0) * x1 + eigen_H1(1,1)*y1 + eigen_H1(1,2);
-        double dz1 = eigen_H1(2,0) * x1 + eigen_H1(2,1)*y1 + eigen_H1(2,2);
-        double len1 = std::sqrt(dx1*dx1 + dy1*dy1 + dz1*dz1);
-        dx1/=len1; dy1/=len1; dz1/=len1;
-        double dx2 = eigen_H2(0,0) * x2 + eigen_H2(0,1)*y2 + eigen_H2(0,2);
-        double dy2 = eigen_H2(1,0) * x2 + eigen_H2(1,1)*y2 + eigen_H2(1,2);
-        double dz2 = eigen_H2(2,0) * x2 + eigen_H2(2,1)*y2 + eigen_H2(2,2);
-        double len2 = std::sqrt(dx2*dx2 + dy2*dy2 + dz2*dz2);
-        dx2/=len2; dy2/=len2; dz2/=len2;
-        double multiply = 1;
-        double ddx = multiply * (dx1-dx2);
-        double ddy = multiply * (dy1-dy2);
-        double ddz = multiply * (dz1-dz2);
-        residuals[0] = ddx;
-        residuals[1] = ddy;
-        residuals[2] = ddz;
+
+        out1 = eigen_H1*eigen_p1;
+        out1/=std::sqrt((out1[0]*out1[0] + out1[1]*out1[1] + out1[2]*out1[2]));
+        out2 = eigen_H2*eigen_p2;
+        out2/std::sqrt((out2[0]*out2[0] + out2[1]*out2[1] + out2[2]*out2[2]));
+        out1-=out2;
+        cout << out1 << endl;
+        residuals[0] = out1[0];
+        residuals[1] = out1[1];
+        residuals[2] = out1[2];
         return true;
     }
     // Factory to hide the construction of the CostFunction object from
     // the client code.
     static ceres::CostFunction* Create(
-            const double x1,
-            const double y1,
-            const double x2,
-            const double y2,
-            double *K1,
-            double *K2) {
-        return (new ceres::NumericDiffCostFunction<ReprojectionError,ceres::CENTRAL, 3, 3, 3>(new ReprojectionError(x1,y1,x2,y2,K1,K2)));
+            const double *p1,
+            const double *p2,
+            const double *K1,
+            const double *K2) {
+        return (new ceres::NumericDiffCostFunction<ReprojectionError,ceres::CENTRAL, 3, 3, 3>(new ReprojectionError(p1,p2,K1,K2)));
     }
 
-    double x1;
-    double y1;
-    double x2;
-    double y2;
-    double *K1;
-    double *K2;
+    const double *p1;
+    const double *p2;
+    const double *K1;
+    const double *K2;
 };
 
 //Need to re-done
@@ -164,8 +139,21 @@ void doingBundle(vector<ImageFeatures> features,vector<MatchesInfo> pairs,vector
     }
     cout << "Ending create rotation array" << endl;
     cout << "Ending create K Matrix" << endl;
+    double p1[rpSet.size()*3];
+    double p2[rpSet.size()*3];
+    for(int i = 0 ; i < rpSet.size();i++){
+        p1[i*3] = rpSet[i].points[0].x;
+        p1[i*3+1] = rpSet[i].points[0].y;
+        p1[i*3+2] = 1;
+        p2[i*3] = rpSet[i].points[1].x;
+        p2[i*3+1] = rpSet[i].points[1].y;
+        p2[i*3+2] = 1;
+    }
     for(int i = rpSet.size() -1 ; i >= 0  ; i--){
-        ceres::CostFunction* cost_func = ReprojectionError::Create(rpSet[i].points[0].x,rpSet[i].points[0].y,rpSet[i].points[1].x,rpSet[i].points[1].y,
+        double *p1_pointer = (p1+(i*3));
+        double *p2_pointer = (p2+(i*3));
+
+        ceres::CostFunction* cost_func = ReprojectionError::Create(p1_pointer,p2_pointer,
                                                                    K_pointer + rpSet[i].matches_image_idx[0]*9,
                                                                    K_pointer + rpSet[i].matches_image_idx[1]*9);
         problem.AddResidualBlock(cost_func,NULL,rotation_pointer + rpSet[i].matches_image_idx[0]*3
